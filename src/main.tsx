8b8431a0c17766a3fe96262fae1ac799ff9f1b47
Leon-Degel-Koehn <leon.koehn2002@gmail.com>
2025-03-20 07:43:21 +0100

    Add bell state display capabilities

--- src/main.tsx
+++ src/main.tsx
@@ -1,49 +1,49 @@
-import { Devvit } from "@devvit/public-api";
-import { LevelScreen } from "./components/level_screen.js";
-import { TestLevel } from "./levels/simplest.js";
-import { Session } from "./levels/types.js";
+import { Devvit } from '@devvit/public-api'
+import { LevelScreen } from './components/level_screen.js';
+import { TestLevel } from './levels/simplest.js';
+import { Session } from './levels/types.js';
+import { BellStateTestLevel } from './levels/bell_state_test.js';
+
 
 Devvit.configure({
-    redis: true,
-    redditAPI: true,
+  redis: true,
+  redditAPI: true,
 });
 
 // Add a menu item to the subreddit menu for instantiating the new experience post
 Devvit.addMenuItem({
-    label: "Add qubitect playtest post",
-    location: "subreddit",
-    forUserType: "moderator",
-    onPress: async (_event, context) => {
-        const { reddit, ui } = context;
-        ui.showToast(
-            "Submitting your post - upon completion you'll navigate there.",
-        );
+  label: 'Add qubitect playtest post',
+  location: 'subreddit',
+  forUserType: 'moderator',
+  onPress: async (_event, context) => {
+    const { reddit, ui } = context;
+    ui.showToast("Submitting your post - upon completion you'll navigate there.");
 
-        const subreddit = await reddit.getCurrentSubreddit();
-        const post = await reddit.submitPost({
-            title: "Test qubitect post",
-            subredditName: subreddit.name,
-            // The preview appears while the post loads
-            preview: (
-                <vstack height="100%" width="100%" alignment="middle center">
-                    <text size="large">Loading ...</text>
-                </vstack>
-            ),
-        });
-        ui.navigateTo(post);
-    },
+    const subreddit = await reddit.getCurrentSubreddit();
+    const post = await reddit.submitPost({
+      title: 'Test qubitect post',
+      subredditName: subreddit.name,
+      // The preview appears while the post loads
+      preview: (
+        <vstack height="100%" width="100%" alignment="middle center">
+          <text size="large">Loading ...</text>
+        </vstack>
+      ),
+    });
+    ui.navigateTo(post);
+  },
 });
 
 Devvit.addCustomPostType({
-    name: "Qubitect",
-    height: "tall",
-    description: "Bite-sized quantum computing puzzles",
-    render: () => {
-        return (
-            //TODO: implement other screens etc.
-            <LevelScreen session={new Session(TestLevel)} />
-        );
-    },
-});
+  name: 'Qubitect',
+  height: 'tall',
+  description: 'Bite-sized quantum computing puzzles',
+  render: () => {
+    return (
+      //TODO: implement other screens etc.
+      <LevelScreen session={new Session(BellStateTestLevel)} />
+    )
+  }
+})
 
-export default Devvit;
+export default Devvit
