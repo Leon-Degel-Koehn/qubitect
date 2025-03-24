6251f652dad48f075f6337b692b8471d470ee641
Julian Lindemann <ju.lin@gmx.de>
2025-03-24 18:32:48 +0100

    Post daily puzzle via cronjob

--- src/levels/simplest.tsx
+++ src/levels/simplest.tsx
@@ -27,3 +27,25 @@ export const TestLevel: Level = {
     title: "The X is the Z in the X-Basis 🤯",
     objective: "Create the output state |1>, |1>",
 };
+
+export const Level1: Level = TestLevel;
+export const Level2: Level = {
+    availableGates: [new PauliX(-1), new PauliZ(-1)],
+    inputState: [
+        new Stabilizer(1, [0, 0], [1, 0]),
+        new Stabilizer(1, [0, 0], [0, 1]),
+    ],
+    expectedResult: [
+        new Stabilizer(-1, [0, 0], [1, 0]),
+        new Stabilizer(-1, [0, 0], [0, 1]),
+    ],
+    circuit: new Circuit(2, [
+        new PauliX(0),
+        new Hadamard(1),
+        new PauliZ(1),
+        new Hadamard(1),
+    ]),
+    greyedOutIndices: [2],
+    title: "Test 2",
+    objective: "Create the output state |0>, |1>",
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { Devvit, useState } from "@devvit/public-api";
 import { LevelScreen } from "./components/level_screen.js";
+import { LEVELS } from "./levels/level_db.js";
 import { Session } from "./levels/types.js";
-import { EPR } from "./levels/cnot_test.js";
-import { TestLevel } from "./levels/simplest.js";
 import { HelpScreen } from "./components/help_screen.js";
 
 Devvit.configure({
@@ -36,33 +35,76 @@ Devvit.addMenuItem({
     },
 });
 
-const LEVELS = [TestLevel, EPR];
+Devvit.addSchedulerJob({
+    name: "Daily Qubitect puzzle post",
+    onRun: async (event, context) => {
+        const { reddit } = context;
+        const subreddit = await reddit.getCurrentSubreddit();
+        await reddit.submitPost({
+            title: "Test qubitect post",
+            subredditName: subreddit.name,
+            // The preview appears while the post loads
+            preview: (
+                <vstack height="100%" width="100%" alignment="middle center">
+                    <text size="large">Loading ...</text>
+                </vstack>
+            ),
+        });
+    },
+});
 
+Devvit.addMenuItem({
+    label: "Add daily Qubitect puzzle post",
+    location: "post",
+    onPress: async (event, context) => {
+        // Initialize level id in redis
+        const hasLevelId = await context.redis.exists("currentLevelId");
+        if (!hasLevelId) {
+            await context.redis.set("currentLevelId", "0");
+        }
+        const jobId = await context.scheduler.runJob({
+            name: "Daily Qubitect puzzle post",
+            cron: "*/2 * * * *",
+        });
+    },
+});
+
+const App: Devvit.CustomPostComponent = async ({ redis }) => {
+    // Fetch current level id from redis
+    const levelId =
+        parseInt(
+            await redis.get("currentLevelId").then((value) => value ?? "0"),
+        ) + 1;
+    // Update current level id in redis
+    await redis.set("currentLevelId", levelId.toString());
+    if (levelId >= LEVELS.length) {
+        return <text>No more levels</text>;
+    }
+  const [displayHelp, changeDisplayHelp] = useState(false);
+    const session = new Session(LEVELS[levelId]);
+    return (
+        <zstack height={100} width={100}>
+            {displayHelp ? (
+                <HelpScreen session={session} />
+            ) : (
+                <LevelScreen session={session} />
+            )}
+            <hstack alignment="end" width={100}>
+                <button
+                    icon={displayHelp ? "wiki-ban" : "wiki"}
+                    onPress={() => {
+                        changeDisplayHelp(!displayHelp);
+                    }}
+                />
+            </hstack>
+        </zstack>
+    );
+};
 Devvit.addCustomPostType({
     name: "Qubitect",
     height: "tall",
     description: "Bite-sized quantum computing puzzles",
-    render: () => {
-        const [displayHelp, changeDisplayHelp] = useState(false);
-        const session = new Session(EPR);
-        return (
-            <zstack height={100} width={100}>
-                {displayHelp ? (
-                    <HelpScreen session={session} />
-                ) : (
-                    <LevelScreen session={session} />
-                )}
-                <hstack alignment="end" width={100}>
-                    <button
-                        icon={displayHelp ? "wiki-ban" : "wiki"}
-                        onPress={() => {
-                            changeDisplayHelp(!displayHelp);
-                        }}
-                    />
-                </hstack>
-            </zstack>
-        );
-    },
+    render: App,
 });
 
 export default Devvit;
