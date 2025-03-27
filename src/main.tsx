6fa33a8862e32b84cde493c8d25df5c9156e8721
Leon-Degel-Koehn <leon.koehn2002@gmail.com>
2025-03-27 22:11:29 +0100

    Remove unwanted menu item

--- src/main.tsx
+++ src/main.tsx
@@ -10,32 +10,6 @@ Devvit.configure({
     redditAPI: true,
 });
 
-// Add a menu item to the subreddit menu for instantiating the new experience post
-Devvit.addMenuItem({
-    label: "Add qubitect playtest post",
-    location: "subreddit",
-    forUserType: "moderator",
-    onPress: async (_event, context) => {
-        const { reddit, ui } = context;
-        ui.showToast(
-            "Submitting your post - upon completion you'll navigate there.",
-        );
-
-        const subreddit = await reddit.getCurrentSubreddit();
-        const post = await reddit.submitPost({
-            title: "Test qubitect post",
-            subredditName: subreddit.name,
-            // The preview appears while the post loads
-            preview: (
-                <vstack height="100%" width="100%" alignment="middle center">
-                    <text size="large">Loading ...</text>
-                </vstack>
-            ),
-        });
-        ui.navigateTo(post);
-    },
-});
-
 Devvit.addSchedulerJob({
     name: "Daily Qubitect puzzle post",
     onRun: async (_, context) => {
