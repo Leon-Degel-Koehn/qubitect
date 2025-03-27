28ccfed3abdb9f0dea5b95fab5c78625ddbab5e1
Leon-Degel-Koehn <leon.koehn2002@gmail.com>
2025-03-27 20:43:19 +0100

    Limit width of objective text

--- src/components/level_screen.tsx
+++ src/components/level_screen.tsx
@@ -485,7 +485,9 @@ export const LevelScreen = (props: LevelScreenProps): JSX.Element => {
                     </hstack>
                 </zstack>
                 <vstack grow alignment="center middle">
-                    <text wrap>{props.session.level.objective || ""}</text>
+                    <text wrap maxWidth="80%">
+                        {props.session.level.objective || ""}
+                    </text>
                 </vstack>
                 <GateSelectionMenu session={props.session} state={state} />
             </vstack>
